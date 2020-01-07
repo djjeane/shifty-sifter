04fe0f86d6524b134a60dfedd8b14ec123822c44
Dawson Jeane <dawsonmjeane@gmail.com>
2020-01-06 22:03:31 -0700

    test2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
-const { EmojiFilter, UserHelpers } = require('./modules/index.js');
-const emojiFilter = new EmojiFilter(client);
+//const { EmojiFilter, UserHelpers } = require('./modules/index.js');
+//const emojiFilter = new EmojiFilter(client);
 const notify = require('./modules/notify.js');
 
 client.once('ready', () => {
@@ -11,29 +11,23 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.author.bot) return; //Excludes the bot from reading its own messages
-    if(message.content == '!sort'){
+
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    if (command === 'ping') {
+        message.channel.send('Pong.');
+    }
+    if (command === 'ping') {
+        message.channel.send('Pong.');
+    }
+    if (command === 'sort') {
         message.channel.send('I have heard your message and will reply shortly my son.');
         sortMembers();
     }
-    if(message.content == `${prefix}sort`)
-    {
-        if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-
-        const args = message.content.slice(prefix.length).split(/ +/);
-        const command = args.shift().toLowerCase();
-        if (command === 'ping') {
-            message.channel.send('Pong.');
-        }
-        if (command === 'ping') {
-            message.channel.send('Pong.');
-        }
-        if (command === 'sort') {
-            message.channel.send('I have heard your message and will reply shortly my son.');
-            sortMembers();
-        }
-    }
 });
     //emojiFilter.checkForMessage(message);
 
