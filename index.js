e636114e73c94982b9aaae0a958433fac58abcf7
Dawson Jeane <dawsonmjeane@gmail.com>
2020-01-07 23:30:08 -0700

    fdsa

--- index.js
+++ index.js
@@ -88,7 +88,35 @@ client.on('message', message => {
         {
             if (nWordUser == taggedUser)
             {
-                nWordUser.ban();
+                const member = message.guild.member(taggedUser);
+                if (member) 
+                {
+                    /**
+                     * Ban the member
+                     * Make sure you run this on a member, not a user!
+                     * There are big differences between a user and a member
+                     * Read more about what ban options there are over at
+                     * https://discord.js.org/#/docs/main/master/class/GuildMember?scrollTo=ban
+                     */
+                    member.ban({
+                        reason: 'You said it!',
+                    }).then(() => {
+                        // We let the message author know we were able to ban the person
+                        message.reply(`Successfully banned ${user.tag}`);
+                    }).catch(err => {
+                        // An error happened
+                        // This is generally due to the bot not being able to ban the member,
+                        // either due to missing permissions or role hierarchy
+                        message.reply('I was unable to ban the member');
+                        // Log the error
+                        console.error(err);
+                    });
+                } 
+                else {
+                    // The mentioned user isn't in this guild
+                    message.reply('That user isn\'t in this guild!');
+                }
+                
                 message.channel.send(`${nWordUser} shouldnt have said it.`);
                 nWordCount =0;
                 nWordUser = "";
