83246cfea1e1ec08f6fa6754e107f7be968ae962
Dawson Jeane <dawsonmjeane@gmail.com>
2020-01-10 20:50:38 -0700

    fdsafds

--- commands/temptthegods.js
+++ commands/temptthegods.js
@@ -1,13 +1,14 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
     var KeepPlaying = true;
     var member = message.guild.member(message.author.id);
-
+    var challenger = member;
     while(KeepPlaying){
 
    
         var killNumber = Math.random() * (10 - 1) + 1;
         if(killNumber == 5)
         {
+            message.channel.send('this would have banned')
             // member.ban({
             //     reason: 'You knew the risks!',
             // }).then(() => {
@@ -20,16 +21,20 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
         }
         else
         {
-            const response = await client.awaitReply(msg, "You won this time, challenge a friends honor. Usage: !temptthegods @user");
+            
+            const response = await client.awaitReply(message, "You won this time, challenge a friends honor. Usage: !temptthegods @user");
             if(response != null)
             {
                 console.log(response)
-                KeepPlaying = false;
+                var taggedUser = response.mentions.users.first();
+                member = taggedUser;
             }
             else
             {
-                message.channel.send("Thanks for playing anyway.")
+                KeepPlaying = false;
+
             }
+
 
         }
     }
