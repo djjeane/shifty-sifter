d37785c1bff79b7d3b8302fc4b08f15ef047af62
dawson jeane <dawson_jeane@yahoo.com>
2020-01-12 16:37:19 -0700

    fdsa

---
+++ commands/howmanypoints.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+exports.run = async(client, message, args, level) => { // eslint-disable-line no-unused-vars
+    let points = JSON.parse(fs.readFileSync("./points.json", "utf8"));
+    if(!points[message.author.id])
+    {
+        message.reply(`You have 0 points.`);
+        return;
+    }
+    var userData = points[message.author.id];
+    message.reply(`You have ${userdata.points} points!`)
+    return;
+    
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['spin'],
+    permLevel: "User"
+};
+
+exports.help = {
+    name: "points",
+    category: "Miscellaneous",
+    description: "Shows you have many points you have.",
+    usage: "points",
+};
