479e7ffcde81851c9ba39c605809db891cf7a50e
Dawson Jeane <dawsonmjeane@gmail.com>
2020-02-05 21:27:37 -0700

    gfds

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,7 @@ module.exports = async client => {
   client.logger.log(`${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
 
   // Make the bot "play the game" which is the help command with default prefix.
-  client.user.setActivity(`${client.config.defaultSettings.prefix}help`, {type: "PLAYING"});
+  client.user.setActivity(`Points: ${ await client.GetPoints(message.author.id)}`, {type: "PLAYING"});
   await mongoose.connect('mongodb+srv://djjeane:CleanCode123@siftydb-6b84b.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useFindAndModify: false
