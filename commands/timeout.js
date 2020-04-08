16a325501f9e5de6d2a68e273439ab9c178184ce
Dawson Jeane <dawsonmjeane@gmail.com>
2020-04-08 13:43:02 -0600

    bvcx

--- commands/timeout.js
+++ commands/timeout.js
@@ -1,12 +1,26 @@
 exports.run = (client, message, args, level) => { 
-      
+    if (args.length == 0 || args.length == 1) return;
+    if (args[1] == "null" || args[1] == "undefined" || args[1] == "NaN") {
+        var timeInSeconds = 60;
+    }
+    else
+    {
+        var time = args[1];
+        if (time > 5)
+        {
+            time = 5;
+        }
+        var timeInSeconds = time * 60;
+        
+    }
     var taggedUser = message.mentions.members.first();
     let mute_role = message.guild.roles.find("name","Shh")
-    console.log(mute_role.id)
-    console.log(taggedUser)
+    console.log(timeInSeconds)
 
     taggedUser.addRole(mute_role)
-    setTimeout(() => {taggedUser.removeRole(mute_role); }, 60 * 1000);
+    setTimeout(() => {
+        taggedUser.removeRole(mute_role);
+    }, timeInSeconds * 1000);
 };
 exports.conf = {
     enabled: true,
