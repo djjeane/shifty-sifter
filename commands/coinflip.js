0507e161ac1a0db0e99bff11c091a7c92034ddde
Roane DeKeyzer <roane.g.o.dekeyzer@gmail.com>
2021-10-02 16:48:24 -0500

    working coinflip

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,49 +1,60 @@
 exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
-    if(args.length < 2){
+    if (args.length < 2) {
         message.reply(`Not enought arguments proveded, please refer to the help file.`);
         return;
     }
     //Grab the needed paramaters from the argu from the message
     // const immutable - let mutable
-	let wager = args[0];
-    let prediction = args[1].lowercase;
+    let wager = args[0];
+    let prediction = args[1].toLowerCase();
     let winnings = 0;
-
+    let msg = '';
     //if any of the provided args are not present, stop here
-    if(!wager || !prediction){
-            return;
+    if (!wager || !prediction) {
+        return;
     }
-    let flip = Round(Math.random(),1)+1 // stores 1 or 2 in flip variable;
+
+
+    let flip = getRandomIntCF(1); // stores 1 or 2 in flip variable;
     let result = '';
-        if (flip === 1){ //converts number to heads or tails stores in result
-            result = 'Heads';
-        } else {
-            result = 'Tails';
-        }
-        if (arg[1] === 'heads' && result === 'Heads') {//begin win/loss logic
-            winnings += args[0] * 2;
-            msg = `Congratulations, you won ${winnings.toString}! You have doubled you bet!`;
-        } else if (arg[1] === 'heads' && result === 'Tails') {
-            msg = `Dang it! You lost ${wager.toString}. Better luck next time.`;
-        } else if (arg[1] === 'tails' && result === 'Tails') {
-            winnings += args[0] * 2;
-            msg = `Congratulations, you won ${winnings.toString}! You have doubled you bet!`;
-        } else if (arg[1] === 'tails' && result === 'Heads') {
-            msg = `Dang it! You lost ${wager.toString}. Better luck next time.`;
-        } else {
-            msg = 'error?';
-        }//end of win/loss logic
-    let gainedPoints = winnings;//for my sanity, keeps format of spin the wheel UpdatePoints
-
-
-    client.UpdatePoints(message.author.id,gainedPoints);
-    message.reply(msg);
-
-
-   
-};
-    function coinflip(guess, bet) {
-
-
+    if (flip === 1) { //converts number to heads or tails stores in result
+        result = 'heads';
+    } else {
+        result = 'tails';
     }
-            message.reply(`Your bet could not be placed. Please reference the help file!`);
+    try {
+        if (prediction === result) {//begin win/loss logic
+            winnings += wager * 2;
+            msg = `Congratulations, you won ${winnings}! You have doubled you bet!`;
+        } else {
+            msg = `Dang it! You lost ${wager}. Better luck next time.`
+            winnings -= wager;
+        }
+        message.reply(msg);
+        client.UpdatePoints(message.author.id, winnings);
+    } catch (error) {
+        console.error(error);
+    }
+    //end of win/loss logic
+};
+function getRandomIntCF(max) {
+    return Math.floor((Math.random() * max)+1);
+  }
+
+
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: "User",
+    pointRec: 0
+};
+
+exports.help = {
+    name: "coinflip",
+    category: "gamble",
+    description: "Who doesn't like to gamble?",
+    usage: "wager guess"
+};
+
