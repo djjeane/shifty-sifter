ea75d46fbcc5a796d11d3fa99b2a48ec0c640f92
Roane DeKeyzer <roane.g.o.dekeyzer@gmail.com>
2022-02-12 11:17:27 -0600

    working coin flip disregard previous push(es)

--- commands/Points/coinflip.js
+++ commands/Points/coinflip.js
@@ -9,7 +9,7 @@ module.exports = {
         .setName('flipthecoin')
         .setDescription('Replies with Pong!')
         .addIntegerOption(option => option.setName('amounttobet').setDescription('Enter a number of points to bet.'))
-        .addStringOption(option => option.setName('makeyourchoice').setDescription(`Heads or tails???`).addChoice('Heads',0).addChoice('Tails',1)),
+        .addStringOption(option => option.setName('makeyourchoice').setDescription(`Heads or tails???`).addChoice('Heads','0').addChoice('Tails','1')),
     async execute(interaction) {
         await interaction.deferReply();
 
@@ -19,29 +19,18 @@ module.exports = {
         var gainedPoints = 0;
         var wager = interaction.options.getInteger('amounttobet');
         var outcome = Math.floor(Math.round(Math.random()));
-        var userChoice = interaction.options.getStringOption('makeyourchoice')
+        var userChoice = parseInt(interaction.options.getString('makeyourchoice'))
         if (outcome == userChoice) {
             gainedPoints = 2*wager;
             await interaction.editReply({ content: `You called it! and you have doubled your money gaining ${gainedPoints} Points! You currently have ${points+gainedPoints}.`});
         } else {
             gainedPoints -= wager;
-            await interaction.editReply({ content: `WRONG! Unfortunately you guessed incorrectly. You lost ${gainedPoints} Points! You currently have ${points+gainedPoints}.`});
+            await interaction.editReply({ content: `Not this time, unfortunately you guessed incorrectly. You lost ${Math.abs(gainedPoints)} Points! You currently have ${points+gainedPoints}.`});
         }
         //updates user points in mongo
         Helper.UpdatePoints(interaction.member.user.id, gainedPoints);
-       
-exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: ['flip', 'callthecoin'],
-    permLevel: "User",
-    pointRec: 0
+    }
 };
 
-exports.help = {
-    name: "flipthecoin",
-    category: "Points",
-    description: "Moves all the turds to the U bend.",
-    usage: "flipthecoin",
-};
 
+ 
